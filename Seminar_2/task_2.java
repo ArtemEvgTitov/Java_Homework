df1ab1a603e7ceb08df9007ceec39169cd9557f7
ArtemEvgTitov <>
2022-10-24 00:10:20 +0300

    Решена вторая задача

--- Seminar_2/task_2.java
+++ Seminar_2/task_2.java
@@ -1,7 +1,27 @@
 package Seminar_2;
 
+import java.util.Arrays;
+
 // Реализовать алгоритм сортировки вставками
 
 public class task_2 {
-    
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && temp < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = { 1, 5, 2, 9, 43, 5, 7, 1 };
+        myArray = insertionSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+
 }
